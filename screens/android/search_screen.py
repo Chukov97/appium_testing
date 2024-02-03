9ac13e25dd526556300783b9a7de187bd55263b1
Ilya Chukov <ilyachuk@gmail.com>
2024-02-03 20:34:35 +0300

    refactoring android screen

--- screens/android/search_screen.py
+++ screens/android/search_screen.py
@@ -6,10 +6,6 @@ class SearchScreen:
     SEARCH_RESULT = (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
     OPEN_SEARCH_RESULT = (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")
 
-    # OPEN_SEARCH_RESULT = (AppiumBy.CLASS_NAME, "android.view.ViewGroup")
-    # OPEN_SEARCH_RESULT = (AppiumBy.XPATH,
-    #                       "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[1]")
-
     def get_search_result(self):
         return browser.all(self.SEARCH_RESULT)
 
