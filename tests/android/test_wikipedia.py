54f2e0c6490a15742499a6cfee6e508366cb7952
Ilya Chukov <ilyachuk@gmail.com>
2024-02-03 20:33:25 +0300

    refactoring android test

--- tests/android/test_wikipedia.py
+++ tests/android/test_wikipedia.py
@@ -1,6 +1,5 @@
 import allure
-from appium.webdriver.common.appiumby import AppiumBy
-from selene import browser, have
+from selene import have
 from screens.android.main_screen import main_screen
 from screens.android.search_screen import search_screen
 
@@ -14,11 +13,5 @@ def test_search():
         results = search_screen.get_search_result()
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
-
-
-def test_open_search_result():
-    with allure.step('Type search'):
-        main_screen.click_search_form()
-        main_screen.input_search_form('Appium')
     with allure.step('Open search result'):
-        search_screen.open_search_result()
+        results.first.click()
