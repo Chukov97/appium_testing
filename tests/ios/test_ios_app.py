ac09144d5867e98931f0a31f148b7ab428db9bc6
Ilya Chukov <ilyachuk@gmail.com>
2024-02-03 20:32:59 +0300

    added test for ios

---
+++ tests/ios/test_ios_app.py
@@ -0,0 +1,12 @@
+import allure
+from screens.ios.main_screen import main_screen
+
+
+def test_search():
+    with allure.step('Click text button'):
+        main_screen.click_text_button()
+    with allure.step('Input text'):
+        main_screen.input_text('Hello world!' + '\n')
+    with allure.step('Check output text'):
+        output_text = main_screen.get_output_text()
+        assert output_text == 'Hello world!'
