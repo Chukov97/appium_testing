51e1f7109ebed78822b8fd1eb656d3e21651799d
Ilya Chukov <ilyachuk@gmail.com>
2024-02-08 01:08:45 +0300

    fixed settings.py and conftest.py

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+from appium.options.android import UiAutomator2Options
+from utils.file import abs_path_from_project
 import pydantic
 
 
@@ -10,7 +12,42 @@ class Config(pydantic.BaseModel):
 
     app: str = os.getenv('APP')
     remote_url: str = os.getenv('REMOTE_URL')
+    udid: str = os.getenv('UDID')
+    device_name: str = os.getenv('DEVICE_NAME')
+    app_wait_activity: str = os.getenv('APP_WAIT_ACTIVITY')
     timeout: float = 10.0
+
+    build_name: str = os.getenv('BUILD_NAME')
+    project_name: str = os.getenv('PROJECT_NAME')
+    session_name: str = os.getenv('SESSION_NAME')
+    platform_version: str = os.getenv('PLATFORM_VERSION')
+
+    def driver_options(self, environment):
+        options = UiAutomator2Options()
+
+        if environment == 'local':
+            options.set_capability('remote_url', self.remote_url)
+            options.set_capability('app', abs_path_from_project(self.app))
+            options.set_capability('udid', self.udid)
+            options.set_capability('deviceName', self.device_name)
+            options.set_capability('appWaitActivity', self.app_wait_activity)
+
+        elif environment == 'bstack':
+            options.set_capability('remote_url', self.remote_url)
+            options.set_capability('app', self.app)
+            options.set_capability('deviceName', self.device_name)
+            options.set_capability('platformVersion', self.platform_version)
+            options.set_capability(
+                'bstack:options', {
+                    'projectName': self.project_name,
+                    'buildName': self.build_name,
+                    'sessionName': self.sessionName,
+                    'userName': self.user_name,
+                    'accessKey': self.access_key,
+                },
+            )
+
+        return options
 
 
 config = Config()
--- tests/android/conftest.py
+++ tests/android/conftest.py
@@ -17,18 +17,42 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management(request):
-    platform = request.config.getoption('--platform')
-    if platform == 'android':
-        options = UiAutomator2Options().load_capabilities({
+    environment = request.config.getoption('--environment')
+    if environment == 'local':
+        options = config.driver_options(environment=environment)
 
-            "platformName": "Android",
-            "appium:deviceName": "Galaxy A22s",
-            "appium:udid": "RZ8T10ZHCEM",
-            "appium:automationName": "UiAutomator2",
-            "appium:app": file.abs_path_from_project("app-alpha-universal-release.apk"),
-            "appium:appWaitActivity": "org.wikipedia.*"
+        with allure.step('Init app session'):
+            browser.config.driver = webdriver.Remote(
+                config.remote_url,
+                options=options
+            )
 
-        })
+        browser.config.timeout = config.timeout
+        browser.config._wait_decorator = support._logging.wait_with(
+            context=allure_commons._allure.StepContext
+        )
+
+        yield
+
+        attach_allure.attach_screenshot()
+        attach_allure.attach_xml()
+        # session_id = browser.driver.session_id
+
+        with allure.step('Tear down app session'):
+            browser.quit()
+
+        # attach_allure.attach_bstack_video(session_id)
+
+        # options = UiAutomator2Options().load_capabilities({
+        #
+        #     "platformName": "Android",
+        #     "appium:deviceName": "Galaxy A22s",
+        #     "appium:udid": "RZ8T10ZHCEM",
+        #     "appium:automationName": "UiAutomator2",
+        #     "appium:app": file.abs_path_from_project("app-alpha-universal-release.apk"),
+        #     "appium:appWaitActivity": "org.wikipedia.*"
+        #
+        # })
     # elif platform == 'ios':
     #     options = XCUITestOptions().load_capabilities({
     #         "deviceName": "iPhone 11 Pro",
@@ -46,25 +70,3 @@ def mobile_management(request):
     #             "accessKey": config.access_key
     #         }
     #     })
-
-    with allure.step('Init app session'):
-        browser.config.driver = webdriver.Remote(
-            config.remote_url,
-            options=options
-        )
-
-    browser.config.timeout = config.timeout
-    browser.config._wait_decorator = support._logging.wait_with(
-        context=allure_commons._allure.StepContext
-    )
-
-    yield
-
-    attach_allure.attach_screenshot()
-    attach_allure.attach_xml()
-    # session_id = browser.driver.session_id
-
-    with allure.step('Tear down app session'):
-        browser.quit()
-
-    # attach_allure.attach_bstack_video(session_id)
